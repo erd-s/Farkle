f013262b4ffbc91fd172381ae4a0c40b451d7dc3
erd-s <chris.erdos@gmail.com>
2016-01-21 14:08:30 -0600

    Initial Commit

---
+++ Yahtzee/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Yahtzee
+//
+//  Created by Chris Erdos on 1/21/16.
+//  Copyright © 2016 Chris Erdos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Yahtzee/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Yahtzee
+//
+//  Created by Chris Erdos on 1/21/16.
+//  Copyright © 2016 Chris Erdos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
